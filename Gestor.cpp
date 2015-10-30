50a768a8411cfbe6566eae5b09d492454d818046
Champetaman <caol29ster@gmail.com>
2015-10-30 10:00:26 -0500

    Se realiza la lectura del archivo de relaciones, sin embargo existe violacion de segmento

--- Gestor.cpp
+++ Gestor.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <cstdlib>
+#include <map>
+#include <vector>
 #include <fstream>
 
 using namespace std;
@@ -11,30 +13,59 @@ struct datosCliente
     map<int, int> Seguidores;
 };
 
-void lectura()
+vector< vector<int> > lectura(ifstream& archivo)
 {
-
+    int x, cont=0, cont2=1, i=0;
+    vector< vector<int> > g;
+    g.clear();
+    while(archivo>>x)
+    {
+		cout<<x;
+		++cont;
+		if(x==1)
+		{
+			g[cont2][i] = cont;	
+			++i;
+		}
+		if(cont==10)
+		{
+			cout<<endl;	
+			cont=0;
+			i=0;
+			++cont2;
+		}
+    }
+    archivo.close();
+    return g;
 }
 
 int main(int argc, char *argv[])
 {
-    int x;
-    map<int, int>datosCliente;
+    map<int, int> datosCliente;
+    vector< vector< int > > g;
     ifstream archivo (argv[1]);
 
     /*============================================
          Verificación de parámetros correctos
     =============================================*/
 
-    if(argc!=4)
+    /*if(argc!=4)
     {
         cout<<"Formato incorrecto. Ingrese de la siguiente manera: [$Cliente] [id] [pipenom]"<<endl;
         exit(0);
-    }
+    }*/
 
-    while(archivo>>x)
+    g = lectura(archivo);
+    
+    cout<<"He aqui el grafo: "<<endl;
+    for(unsigned int i=0; i<g.size();++i)
     {
-        cout<<x;
-    }
+		for(unsigned int j=0; j<g[i].size(); ++j)
+		{
+			cout<<g[i][j]<<" ";
+		}
+	}
+	cout<<endl;
+    
     return 0;
 }
