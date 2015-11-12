e3df044963a2bb430113e9289cb8f6ddffc4df28
Xubuntu <racso0468@hotmail.com>
2015-11-12 09:41:53 -0800

    Arreglado y funcionando :D

--- Cliente.cpp
+++ Cliente.cpp
@@ -1,114 +1,123 @@
 
 #include "datosCliente.h"
 
-/*======================================================================================
-*	Variable que determina el estado del gestor (Sincrono = false | asincrono = true)
-   ======================================================================================*/
-bool estado = false;
-int fd;
+/*============================================
+--inicioSesio--
+Se encarga de conectar al cliente, validarlo
+y retorna el estado en el que está el gestor
+=============================================*/
 
-/*NOS FALTA QUE CUANDO EL GESTOR NOS RESPONDA SABER SI ESTA SINCRONICO O
-   ASINCRONICO Y MOSTRAR LA OPCION 4 O NO*/
+bool inicioSesion(int id, int pG, int pC, string pipeC) {
+  int c;
+  cout<<"..Iniciando sesion.................."<<endl;
 
-bool inicioSesion(int id, string nomPipe,string pipe) {
-        cout<<"Iniciando sesion..."<<endl;
+  datosEnvio dEscritura, dLectura;
+  dEscritura.action = -1;
+  dEscritura.id = id;
+  strcpy(dEscritura.pipename, pipeC.c_str());
+  cout<<": Enviando solicitud a gestor \n";
 
-        int creado, fd1;
-        datosEnvio dE, dR;
+  if(write(pG, &dEscritura, sizeof(dEscritura))==-1){
+    perror("Escritura inicio de sesión");
+    exit(0);
+  }
+  // Codigo agregado
+  do {
+      c =  read(pC, &dLectura, sizeof(dLectura));
+      sleep(1);
+      cout<<" c es "<<c<<endl;
+  } while (c <= 0);
 
-        // manejador de seniales
-        signal(SIGUSR1, (sighandler_t)signalHandler);
+  /*if(read(pC, &dLectura, sizeof(dLectura))==-1){
+    perror("Lectura inicio de sesión");
+    exit(0);
+    }*/
+  cout<<"Modo R="<<dLectura.action<<" Mensaje R="<<dLectura.message<<endl;
+  cout<<": El gestor responde... \n";
+  if(dLectura.action==0)
+  cout<<"Alguien hackeo su cuenta, ya se encuentra abierta."<<endl;
+  else if(dLectura.action==1){
+    cout<<"Ha iniciado sesion correctamente!"<<endl;
+    cout<<"El servidor se encuentra en estado: "<<dLectura.message<<endl;
+  }
 
-        do {
-                fd1 = open (nomPipe.c_str(), O_WRONLY);
-                if(fd1 == -1) {
-                        perror("pipe");
-                        printf(" Se volvera a intentar despues\n");
-                        sleep(5);
-                }
-        } while(fd1 == -1);
-
-        dE.action = -1;
-        dE.id = id;
-        dE.pid = getpid();
-
-        strcpy(dE.pipename, pipe.c_str());
-        cout<<"Enviando solicitud a gestor \n";
-        write(fd1, &dE, sizeof(dE));
-        //Pause del manejador de seniales
-        cout<<"Si necesitas reggaeton pauseame \n";
-        pause();
-        cout<<"Te leo el juanete"<<endl;
-        read(fd, &dR, sizeof(dR));
-        cout<<"Lo lei \n";
-        //cout<<dR.action<<endl;
-        if(dR.action==0)
-                cout<<"Alguien hackeo su cuenta, ya se encuentra abierta."<<endl;
-        else if(dR.action==1){
-                cout<<"Ha iniciado sesion correctamente!"<<endl;
-                cout<<"El servidor se encuentra en estado: "<<dR.message<<endl;
-        }
-        close(fd1);
 }
 
 int main(int argc, char **argv)
 {
-        int arg2,opciones = 0;
-        string arg=argv[1];
-        arg2=atoi(argv[1]);
-        string nompipeGC;
-        mode_t fifo_mode = S_IRUSR | S_IWUSR;
+  int pipeG, pipeC, creado=0;
+  int argv1,opciones = 0;
+  argv1=atoi(argv[1]);
+  string nompipeGC;
 
-        /*=============================================
-           Verificación de parámetros correctos
-           ===============================================*/
-        if(argc!=3)
-        {
-                cout<<"Formato incorrecto. Ingrese de la siguiente manera: [$Cliente] [id] [pipenom]"<<endl;
-                exit(0);
-        }
-        /*============================================
-           VERIFICACION USUARIO
-           Se verifica si el id es valido
-           =============================================*/
+  mode_t fifo_mode = S_IRUSR | S_IWUSR;
 
-        if(arg2< 1 || arg2> 10)
-        {
-                cout << "No es posible conectarse con el id que indica, Por favor ingrese un numero entre 1 y 10." <<endl;
-                exit(0);
-        }
+  /*=============================================
+  Verificación de parámetros correctos
+  ===============================================*/
+  if(argc!=3)
+  {
+    cout<<"Formato incorrecto. Ingrese de la siguiente manera: [$Cliente] [id] [pipenom]"<<endl;
+    exit(0);
+  }
+  /*============================================
+  VERIFICACION USUARIO:  Se verifica si el id es valido
+  =============================================*/
 
-        //Creacion del pipe de lectura para recibir informacion del gestor
-        // Nombre de un segundo pipe
-        //strcpy(datos.segundopipe, "mypipe");
-        // Se crea un segundo pipe
-        nompipeGC = argv[2];
-        nompipeGC += (argv[1]);
+  if(argv1< 1 || argv1> 10)
+  {
+    cout << "No es posible conectarse con el id que indica, Por favor ingrese un numero entre 1 y 10." <<endl;
+    exit(0);
+  }
+  /*============================================
+  Creacion de pipes
+  =============================================*/
+  // Pipe gestor
+  do {
+    pipeG = open (argv[2], O_WRONLY);
+    if(pipeG == -1) {
+      perror("pipe");
+      printf(" Se volvera a intentar despues\n");
+      sleep(5);
+    }
+    else creado=1;
+  } while(creado == 0);
 
-        unlink(nompipeGC.c_str());
-        if (mkfifo (nompipeGC.c_str(), fifo_mode) == -1) {
-                perror("cliente mkfifo");
-                exit(1);
-        }
-        // Se abre el segundo pipe
-        if ((fd = open(nompipeGC.c_str(), O_RDONLY|O_NONBLOCK)) == -1) {
-                perror("Cliente abriendo el segundo pipe");
-                exit(0);
-        }
 
-        if(!inicioSesion(atoi(argv[1]),argv[2],nompipeGC)) {
-                cout<<"Se ha iniciado sesion correctamente"<<endl;
-        }
-        else{
-                cout<<"El usuario con el que esta intentando ingresar ya esta conectado"<<endl;
-                exit(0);
-        }
-        /*============================================
-           SECCION ID
-           =============================================*/
-        //fd = open (argv[2], O_WRONLY);
-        //while(menu(opciones, fd)) ;
-        //close(fd);
+  nompipeGC = argv[2];
+  nompipeGC += (argv[1]);
 
-        return 0;
+  unlink(nompipeGC.c_str());
+
+  if (mkfifo (nompipeGC.c_str(), fifo_mode) == -1) {
+    perror("cliente mkfifo");
+    exit(1);
+  }
+
+  if ((pipeC = open(nompipeGC.c_str(), O_RDONLY|O_NONBLOCK)) == -1) {
+    perror("Cliente abriendo el segundo pipe");
+    exit(0);
+  }
+
+
+
+
+  /*============================================
+  Operaciones cliente
+  =============================================*/
+
+  if(!inicioSesion(atoi(argv[1]),pipeG,pipeC,nompipeGC)) {
+    cout<<"Se ha iniciado sesion correctamente"<<endl;
+  }
+  else{
+    cout<<"El usuario con el que esta intentando ingresar ya esta conectado"<<endl;
+    exit(0);
+  }
+
+  while(true) {
+    sleep(2);
+  }
+  //close(fd);
+
+  return 0;
 }
--- Gestor.cpp
+++ Gestor.cpp
@@ -1,155 +1,142 @@
 #include "datosCliente.h"
 
-bool mode;
-set< int > conectados;
-int fd;
 
-void datosEntrada( datosEnvio opcion, vector<datosCliente>  dc)
+void datosEntrada( datosEnvio opcion, vector<datosCliente>  dc, string mode, set< int > conectados)
 {
-								int decizione = opcion.action;
-								datosEnvio dE;
+	int decizione = opcion.action;
+	datosEnvio dE;
 
-								switch(decizione)
-								{
-								case -1:
-								{
-																cout<<"Gestor evaluando sesion de "<<opcion.id<<endl;
-																int fd1, verificar = 0;
-																string aux;
-																if(mode==true)
-																								aux="sincrono";
-																else aux="asincrono";
-																if(conectados.find(opcion.id) == conectados.end()) {
-																								verificar = 1;
-																								strcpy(dE.message,aux.c_str() );
-																								conectados.insert(opcion.id);
-																}
-																dE.action = verificar;
+	switch(decizione)
+	{
+		case -1:
+		{
+			cout<<"**Gestor evaluando sesion de "<<opcion.id<<endl;
+			int pipeC, verificar = 0;
+			if(conectados.find(opcion.id) == conectados.end()) {
+				verificar = 1;
+				strcpy(dE.message,mode.c_str() );
+				conectados.insert(opcion.id);
+			}
+			dE.action = verificar;
 
-																if ((fd1 = open(opcion.pipename, O_WRONLY|O_NONBLOCK)) == -1) {
-																								perror("Gestor Abriendo el pipe de respuesta");
-																								exit(0);
-																}
-																cout<<"El gestor enviando respuesta de la sesion\n";
-																cout<<opcion.pipename<<" "<<dE.message<<" "<<dE.action<<endl;
-																cout<<kill(opcion.pid, SIGUSR1)<<endl;
-																write(fd1, &dE, sizeof(dE));
+			if ((pipeC = open(opcion.pipename, O_WRONLY|O_NONBLOCK)) == -1) {
+				perror("Gestor Abriendo el pipe de respuesta");
+				exit(0);
+			}
+			cout<<"**El gestor enviando respuesta de la sesion\n";
+			cout<<opcion.pipename<<" "<<dE.message<<" "<<dE.action<<endl;
 
-																close(fd1);
-								} break;
-								case 1:
-								{
+			if(write(pipeC, &dE, sizeof(dE))==-1){
+				perror("Gestor escribiendo en el pipe Cliente");
+				exit(0);
+			}
 
-								} break;
+			cout<<"termina\n";
+		} break;
 
-								case 2:
-								{
+		default:
+		{
 
-								} break;
-
-								case 3:
-								{
-
-								} break;
-
-								case 4:
-								{
-
-								} break;
-
-								default:
-								{
-
-								} break;
+		} break;
 
 
-								}
+	}
+	cout<<"sale"<<endl;
 }
+
+
+/*============================================
+--lectura--
+Lee el archivo
+=============================================*/
 
 void lectura(vector<datosCliente> &g,string nom)
 {
-								ifstream archivo (nom.c_str());
-								if(archivo.is_open()) {
-																int x, cont=0, cont2=0;
-																while(archivo>>x)
-																{
-																								if(x==1)
-																								{
-																																g[cont2].follow[cont] = 0;
+	ifstream archivo (nom.c_str());
+	if(archivo.is_open()) {
+		int x, cont=0, cont2=0;
+		while(archivo>>x)
+		{
+			if(x==1)
+			{
+				g[cont2].follow[cont] = 0;
 
-																								}
-																								++cont;
-																								if(cont == 10)
-																								{
-																																cont = 0;
-																																++cont2;
-																								}
+			}
+			++cont;
+			if(cont == 10)
+			{
+				cont = 0;
+				++cont2;
+			}
 
-																}
-																archivo.close();
-																cout<<"Lettura al 100%"<<endl;
-								}
-								else{
-																cout<<"Lettura del file è sbagliato"<<endl;
-																exit(0);
-								}
+		}
+		archivo.close();
+		cout<<"Lettura al 100%"<<endl;
+	}
+	else{
+		cout<<"Lettura del file è sbagliato"<<endl;
+		exit(0);
+	}
 }
 
 int main (int argc, char **argv)
 {
 
-								int pid, n, bytes;
-								string sin=argv[2];
-								datosEnvio dEe;
-								vector<datosCliente> datos(10);
+	int pid, pipeG,pipeC;
+	string modo=argv[2];
+	datosEnvio dRecibido;
+	vector<datosCliente> datos(10);
+	set< int > conectados;
 
-								/*=============================================
-								   Verificación de parámetros correctos
-								   ===============================================*/
+	/*=============================================
+	Verificación de parámetros correctos
+	===============================================*/
 
-								if(argc!=4)
-								{
-																cout<<"Formato incorrecto. Ingrese de la siguiente manera: [$Gestor] [relaciones] [modo] [pipenom]"<<endl;
-																exit(0);
-								}
+	if(argc!=4)
+	{
+		cout<<"Formato incorrecto. Ingrese de la siguiente manera: [$Gestor] [relaciones] [modo] [pipenom]"<<endl;
+		exit(0);
+	}
 
-								if(sin=="sincrono")  mode=true;
-								else mode=false;
+	/*=============================================
+	Lectura del archivo de relaciones
+	===============================================*/
 
-
-								/*=============================================
-								   Lectura del archivo de relaciones
-								   ===============================================*/
-
-								cout<<"Lettura di file Relazioni... ";
-								lectura(datos, argv[1]);
+	cout<<"Lettura di file Relazioni... ";
+	lectura(datos, argv[1]);
 
 
-								/*=============================================
-								   Iniciando pipe gestor
-								   ===============================================*/
-								mode_t fifo_mode = S_IRUSR | S_IWUSR;
+	/*=============================================
+	Iniciando pipe gestor
+	===============================================*/
+	mode_t fifo_mode = S_IRUSR | S_IWUSR;
 
-								unlink(argv[3]);
-								if (mkfifo (argv[3], fifo_mode) == -1) {
-																perror("mkfifo");
-																exit(1);
-								}
+	unlink(argv[3]);
+	if (mkfifo (argv[3], fifo_mode) == -1) {
+		perror("mkfifo");
+		exit(1);
+	}
 
-								sleep(5);
+	sleep(5);
 
-								fd = open (argv[3], O_RDONLY);
-								printf ("Pipe aperto\n");
+	pipeG = open (argv[3], O_RDONLY);
+	printf ("Pipe aperto\n");
+	int aux;
+	/*=============================================
+	Gestor escuchando
+	===============================================*/
+	while((aux=read(pipeG, &dRecibido, sizeof(dRecibido))!=-1))
+	{
+		cout<<aux<<endl;
+		if(aux>0){
+			cout<<"entro\n";
 
-								/*=============================================
-								   Gestor escuchando
-								   ===============================================*/
-								while(read(fd, &dEe, sizeof(dEe)))
-								{
+			datosEntrada(dRecibido, datos, modo, conectados);
+		}
+		sleep(5);
+		cout<<"llega\n";
 
-																datosEntrada(dEe, datos);
+	}
+	cout<<aux<<endl;
 
-								}
-
-								close(fd);
 }
